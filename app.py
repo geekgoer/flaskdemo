736098999d8037ec9065385f618e2ce8bf4afd2b
codechen <2832404468@qq.com>
2023-06-01 17:30:21 +0800

    Add base template add error(404) template

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', movies= movies, user= user)
+    return render_template('index.html', movies= movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -53,6 +52,15 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo('Done.')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
 class User(db.Model):
     id= db.Column(db.Integer,primary_key=True)
