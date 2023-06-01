8e77dde881acf03407857bb3116d24e5184179b2
codechen <2832404468@qq.com>
2023-06-01 16:01:37 +0800

    Add database support with Flask-SQLALchemy

--- app.py
+++ app.py
@@ -1,28 +1,72 @@
+import os
+import sys
+import click
+
 from flask import Flask, render_template
 from flask import url_for
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
 
+WIN= sys.platform.startswith('win')
+if WIN :
+    prefix='sqlite:///'
+else :
+    prefix='sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', movies= movies, name= name)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', movies= movies, user= user)
 
 @app.route('/user/<name>')
 def user_page(name):
     return f'User:{escape(name)}'
 
-name = 'alex'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+@app.cli.command()
+def forge():
+    db.create_all()
+
+    name = 'alex'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title = m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+class User(db.Model):
+    id= db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+
+class Movie(db.Model):
+    id= db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year= db.Column(db.String(4))
+
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database")
