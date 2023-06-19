37acc54a52a298142f911f1c510e4ccdffd45773
codechen <2832404468@qq.com>
2023-06-19 14:36:00 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -19,9 +19,11 @@ else :
     prefix='sqlite:////'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),os.getenv('DATASET_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev')
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
